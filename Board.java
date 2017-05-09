feabe1b30a49a73ba52a9a2bd274903dd4bac0b3
Hannah Fink <Hannah.M.Fink@gmail.com>
2017-05-09 14:54:23 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -16,18 +16,20 @@ public class Board{
       } else if (strcmp(keyword, takeRole)==0) {
          // when a player takes a role
          // get role name
-         String name = ;
+         String name = getName();
          System.out.println("Players new role is, %s", name);
       } else if (strcmp(keyword, endScene)==0) {
          // when a scene ends and all players are moved off of the roles
          System.out.println("All players in ended scene have moved to, (%d, %d)", x, y);
       } else {
          // keyword not recognized display error and try again
+         System.out.println("System error: Keyword not recognized. Please try again.");
       }
    }
 
    // display a message to the user(s) on the Board
    public void dispMessage(String message){
-      // print message to the terminal or text window?
+      // print message
+      System.out.println(message);
    }
 }// end Board class
