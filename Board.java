ae102064856bd0a969af6005683b8ec4734bdb45
Hannah Fink <Hannah.M.Fink@gmail.com>
2017-05-09 14:50:00 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -6,8 +6,28 @@ public class Board{
    public Board(){}
 
    // update the Board display
-   public void update(){}
+   public void update(String keyword, int x, int y){
+      if (strcmp(keyword, position)==0) {
+         // when a players position needs to be updated
+         System.out.println("Players new position is, (%d, %d)", x, y);
+      } else if (strcmp(keyword, level)==0) {
+         // when a players level is upgraded
+         System.out.println("Players old level was %d and new level is %d)", x, y);
+      } else if (strcmp(keyword, takeRole)==0) {
+         // when a player takes a role
+         // get role name
+         String name = ;
+         System.out.println("Players new role is, %s", name);
+      } else if (strcmp(keyword, endScene)==0) {
+         // when a scene ends and all players are moved off of the roles
+         System.out.println("All players in ended scene have moved to, (%d, %d)", x, y);
+      } else {
+         // keyword not recognized display error and try again
+      }
+   }
 
    // display a message to the user(s) on the Board
-   public void dispMessage(String message){}
+   public void dispMessage(String message){
+      // print message to the terminal or text window?
+   }
 }// end Board class
