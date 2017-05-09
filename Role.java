9d5c0bcd86d1d16dc6b6076ed28d28243d9df2bf
Hannah Fink <Hannah.M.Fink@gmail.com>
2017-05-09 14:53:08 -0700

    Update Role.java

--- Role.java
+++ Role.java
@@ -2,21 +2,27 @@ public class Role {
 	// Fields
 	int playerNum;
 	int roleLevel;
+	String roleName;
 	boolean mainOrExtra; // true if main role false if extra
 	// Constructor
-	Role (int level, boolean onCard) {
+	Role (int level, boolean onCard, String name) {
 		roleLevel = level;
 		mainOrExtra = onCard;
+		roleName = name;
 	}
 	// Methods
-	void AddPlayer (int num, int level) {
+	public void AddPlayer (int num, int level) {
 		if (level >= roleLevel) {
 			playerNum = num;
 		} else {
 			// display error message and continue
 		}
 	}
-	boolean MainOrExtra () {
+	public boolean MainOrExtra () {
 		return mainOrExtra;
+	}
+	
+	public String getName () {
+		return roleName;
 	}
 }
