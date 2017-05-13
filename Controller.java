368548aba535a6e2900945d377d38092e8ff710c
Trevor Donnelly <tdonnelly1586@gmail.com>
2017-05-13 16:20:09 -0700

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,10 +1,13 @@
 // Controller.java
 
+import java.util.*;
+
 public class Controller{
 
    private int day = 0;
    private int sceneCount = 0;
    private int playerCount = 0;
+   private Arraylist<Card> cards;
 
    // Controller constructor
    public Controller(int num){
@@ -15,17 +18,13 @@ public class Controller{
       createPlayers();
       createRooms();
    }
-   
+
    private void createPlayers () {
 	   for (int i=0; i<playerCount; i++) {
-		   
+
 	   }
    }
-   
-   private void createCards () {
-	   
-   }
-   
+
    private void createRooms () {
 	   // create all rooms
 	   CastingOffice office = new CastingOffice(playerCount);
@@ -50,5 +49,105 @@ public class Controller{
    public void endGame () {}
 
    public void calcWinner () {}
-   
+
+
+   public static void createCards(){
+
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      try{
+         DocumentBuilder builder = factory.newDocumentBuilder();
+         Document doc  = builder.parse("cards.xml");
+
+         // creates a NodeList of every element in the .xml file
+         NodeList cardList = doc.getElementsByTagName("*");
+
+         int cardCount = 0;
+         String name = "";
+         String budget = "";
+         String sceneNum = "";
+         String sceneLine = "";
+
+         String roleName = "";
+         String roleLevel = "";
+         String roleLine = "";
+
+         boolean create = false;
+
+         int createCount = 0;
+
+         // iterates through the elements of the .xml file and creates
+         // card object using the xml tags as references
+         for (int i = 0; i < cardList.getLength(); i++){
+
+            Node cardNode = cardList.item(i);
+            Element card = (Element) cardNode;
+
+            // card section
+            if (card.getTagName().equals("card")){
+
+               if (create){
+                  int budg = Integer.valueOf(budget);
+                  int num = Integer.valueOf(sceneNum);
+                  Card current = new Card(name, budg, num, sceneLine, roles);
+                  cards.add(current);
+                  roles.clear();
+                  createCount++;
+               }
+               name = card.getAttribute("name");
+               budget = card.getAttribute("budget");
+               create = true;
+
+/*
+               System.out.println("------------------------------------------");
+               System.out.println("Name of card: " + name);
+               System.out.println("Budget of card: " + budget);
+               cardCount++;
+*/
+            }
+
+            //scene section
+            else if (card.getTagName().equals("scene")){
+               cardNode = cardList.item(i);
+               card = (Element) cardNode;
+               sceneNum = card.getAttribute("number");
+               sceneLine = card.getTextContent();
+
+/*             System.out.println("scene number: " + sceneNum);
+               System.out.println("scene description: " + sceneLine);
+*/
+            }
+
+            // parts sections
+            else if (card.getTagName().equals("part")){
+               roleName = card.getAttribute("name");
+               roleLevel = card.getAttribute("level");
+               roleLine = card.getTextContent();
+
+               int level = Integer.valueOf(roleLevel);
+
+               // build a Role ArrayList to pass to the card
+               Role temp = new Role(level, true, roleName, roleLine);
+               roles.add(temp);
+/*
+               System.out.println("role name: " + roleName);
+               System.out.println("role level: " + roleLevel);
+               System.out.println("role line: " + roleLine);
+*/
+            }
+         }
+         System.out.println("total cards created = " + createCount);
+         return cards;
+      }
+      catch(ParserConfigurationException ex){
+         ex.printStackTrace();
+      }
+      catch(SAXException ex){
+         ex.printStackTrace();
+      }
+      catch(IOException ex){
+         ex.printStackTrace();
+      }
+
+   }
+
 }// end Controller class
--- Role.java
+++ Role.java
@@ -3,13 +3,17 @@ public class Role {
 	int playerNum;
 	int roleLevel;
 	String roleName;
+	String line;
 	boolean mainOrExtra; // true if main role false if extra
+
 	// Constructor
-	Role (int level, boolean onCard, String name) {
+	Role (int level, boolean onCard, String name, String line) {
 		roleLevel = level;
 		mainOrExtra = onCard;
 		roleName = name;
+		this.line = line;
 	}
+
 	// Methods
 	public void AddPlayer (int num, int level) {
 		if (level >= roleLevel) {
@@ -18,10 +22,11 @@ public class Role {
 			// display error message and continue
 		}
 	}
+
 	public boolean MainOrExtra () {
 		return mainOrExtra;
 	}
-	
+
 	public String getName () {
 		return roleName;
 	}
