90955f5250c70cb5f9ed96307128e99952dd757a
Trevor Donnelly <tdonnelly1586@gmail.com>
2017-05-13 16:18:32 -0700

    Add files via upload

--- Card.java
+++ Card.java
@@ -5,41 +5,20 @@ import java.util.*;
 public class Card{
 
    private String name;
-   private boolean flipped;
    private int budget;
-   private ArrayList<Role> roles = new ArrayList<Role>();
+   private int sceneNum;
+   private String description;
+   private ArrayList<Role> roles;
+   private boolean flipped;
 
-   // constructor for single role cards
-   public Card(String name, int budget, Role role0){
+   // constructor
+   public Card(String name, int budget, int sceneNum, String description, ArrayList<Role> roles){
       this.name = name;
       this.budget = budget;
+      this.sceneNum = sceneNum;
+      this.description = description;
       flipped = false;
-
-      // add the given role to the arraylist of roles for the card
-      roles.add(role0);
-   }
-
-   // constructor for double role cards
-   public Card(String name, int budget, Role role0, Role role1){
-      this.name = name;
-      this.budget = budget;
-      flipped = false;
-
-      // add the given roles to the arraylist of roles for the card
-      roles.add(role0);
-      roles.add(role1);
-   }
-
-   // constructor for triple role cards
-   public Card(String name, int budget, Role role0, Role role1, Role role2){
-      this.name = name;
-      this.budget = budget;
-      flipped = false;
-
-      // add the given roles to the arraylist of roles for the card
-      roles.add(role0);
-      roles.add(role1);
-      roles.add(role2);
+      this.roles = roles;
    }
 
 ////////////////////////////////////////////////////////////////////
@@ -51,14 +30,25 @@ public class Card{
       return flipped;
    }
 
-   public int getBudget(){
-      return budget;
-   }
-
    public String getName(){
       return name;
    }
 
+   public int getBudget(){
+      return budget;
+   }
+
+   public int getSceneNum(){
+      return sceneNum;
+   }
+
+   public String getDescription(){
+      return description;
+   }
+
+   public ArrayList<Role> getRoles(){
+      return roles;
+   }
    public void flip(){
       flipped = true;
    }
