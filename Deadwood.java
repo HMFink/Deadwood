c10cddb6ae81fc94f4c61ab7c86930bf467e4554
Hannah Fink <Hannah.M.Fink@gmail.com>
2017-05-15 10:34:24 -0700

    Update Deadwood.java

--- Deadwood.java
+++ Deadwood.java
@@ -15,5 +15,19 @@ public class Deadwood {
 		// Call controller
 		startGame(numPlaying);
 	}
-
+/* NOTES *****************************************************************************************************
+ * Command				Action
+ * ***********************************************************************************************************
+ * who					The software identifies the current player and any parts that the player is working.
+ * Where				The software describes the current player’s room and any active scenes.
+ * move (room)			The current player moves to the indicated room.
+ * work (part)			The current player takes the indicated role.
+ * upgrade ($ level) 	Upgrade the current player to the indicated level
+ * upgrade (cr level)	Upgrade the current player to the indicated level.
+ * Rehearse 			The current player rehearses
+ * act 					The current player performs in its current role.
+ * end					End the current player’s turn 
+ * 
+ * HELP: http://www.vogella.com/tutorials/SWT/article.html
+ *************************************************************************************************************/
 }
