0c29b486412a10c68ebd672df284347dd24cb3d2
Hannah Fink <Hannah.M.Fink@gmail.com>
2017-05-31 11:53:39 -0700

    Update Deadwood.java

--- Deadwood.java
+++ Deadwood.java
@@ -4,40 +4,20 @@
 
 
 import java.util.*;
+import javax.swing.JFrame;
+import java.awt.*;
 
 public class Deadwood{
-
-
-	private static void userOptions(){
-     		System.out.println("----------------------------------------------------------------------------");
-     		System.out.println("Please enter one of the following: ");
-     		System.out.println();
-				System.out.println("'who' = to see the current players information and any role they are working");
-				System.out.println("'where' = to see your current room and any active scenes");
-     		System.out.println("'move' = move to a room adjacent to your current position");
-     		System.out.println("'work' = to take a role in your current room");
-				System.out.println("'rehearse' = to rehearse your current role");
-     		System.out.println("'act' = act out the role you are currently in");
-     		System.out.println("'upgrade' = upgrade your current level in exchange for money or credit");
-				System.out.println("'end' = to end your turn");
-     		System.out.println("----------------------------------------------------------------------------");
-	}
-
 
 	public static void main(String[] args) {
 		// Variables
 		Scanner in = new Scanner(System.in);
 		int numPlaying = 0;
 		int currentPlayer = 1;
-		// Enter number of players
-		System.out.println("****************************************************************************");
-    System.out.println("                                 DEADWOOD                                   ");
-    System.out.println("****************************************************************************");
-		System.out.print("Please enter the number of players (2-3): ");
-		numPlaying = in.nextInt();
-		System.out.println();
-		// Call controller
+		// Call controller and Board
 		Controller control = new Controller(numPlaying);
+		Board gameBoard = new Board();
+		gameBoard.setVisible(true);
 		//control.startGame(numPlaying);
 		// used to determine if the user input was valid or not
 		boolean valid;
